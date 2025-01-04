7f4a3a66ed7e411b0690768190b51eaa68bb2920
Vikash . <134270137+GitGuruCode@users.noreply.github.com>
2025-01-04 22:11:18 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,22 @@
-import 'reflect-metadata'; 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Swagger Configuration
+  const config = new DocumentBuilder()
+    .setTitle('College Management API')
+    .setDescription('API documentation for College Management System')
+    .setVersion('1.0')
+    .addTag('Colleges')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(3000);
+  console.log('Application is running on http://localhost:3000');
+  console.log('Swagger docs available at http://localhost:3000/api-docs');
 }
 bootstrap();
