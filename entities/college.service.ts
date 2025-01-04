e74d68c784e4647c0748d6ca7192bc1ae1d3a70e
Vikash . <134270137+GitGuruCode@users.noreply.github.com>
2025-01-04 22:05:01 +0530

    Update college.service.ts

--- entities/college.service.ts
+++ entities/college.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class CollegeService {
   async getPlacementData(collegeId: number) {
-    // Example response: Replace with actual database logic later
+
     return {
       collegeId,
       placements: [],
